b991d46c63449b47ba93ab8da390c57ecbcaccc3
ibraheem-areeda <ibraheem.areeda@gmail.com>
2025-01-12 10:15:40 +0300

    edit DB

--- hst_ishbik_intgration/controllers/order_controllers.py
+++ hst_ishbik_intgration/controllers/order_controllers.py
@@ -18,7 +18,7 @@ class OrderIntegration(http.Controller):
     @http.route('/api/v1/orders', methods=["POST"], type="http", auth="none", csrf=False)
     def grubtech_order_create(self, **kwargs):
         ###hamiltontech-ishbek17-main-15486349#### db
-        request.session.authenticate("hamiltontech-eshbek171-main-17018402", "api", "api")
+        request.session.authenticate("hamiltontech-eshbek17-main-17595363", "api", "api")
         # request.session.authenticate("test_order", "admin", "admin")
         order:dict = json.loads(request.httprequest.data)
         pos_config = request.env['pos.config'].search_read([("id", "=", order['storeId'])], ['id','current_session_id','name','sequence_id','default_fiscal_position_id','pricelist_id'], limit=1)
