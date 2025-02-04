c4f47908869bf8dd639b815c915cbc85c109380a
ibraheem-areeda <ibraheem.areeda@gmail.com>
2025-02-04 14:29:34 +0300

    ff

--- product_multiple_images_upload/__manifest__.py
+++ product_multiple_images_upload/__manifest__.py
@@ -1,13 +1,11 @@
 {
     'name': 'Product Multiple Images Upload',
     'author': 'Opsway',
-    'version': '17.0',
+    'version': '17.0.1.0.0',
     'website': 'https://www.opsway.com',
     'category': 'Sales',
     'description': 'Module to upload multiple images for a product',
-    'summary': """
-        Module to upload multiple images for a product
-    """,
+    'summary': 'Module to upload multiple images for a product',
     'depends': [
         'web',
         'website_sale',
@@ -18,12 +16,15 @@
     'images': ['static/description/banner.png'],
     'license': 'LGPL-3',
     'assets': {
-        'web.assets_backend': [
-            # 'product_multiple_images_upload/static/src/*.js',
-            # 'product_multiple_images_upload/static/src/*.xml',
-        ],
+'assets': {
+    'web._assets_core_web': [
+        'product_multiple_images_upload/static/src/*.js',
+        'product_multiple_images_upload/static/src/*.xml',
+    ],
+},
+
     },
     'installable': True,
     'auto_install': False,
-    'application': True,
+    'application': False,
 }
